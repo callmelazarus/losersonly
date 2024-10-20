a8750ad3f0515783c33d18fbdfd3ad5ac5815d27
JayG <thatsumpossible@gmail.com>
2024-10-19 23:05:49 -0700

    some css changes, and an attempt to use GH actions for automtically building app

--- losers_only/next.config.mjs
+++ losers_only/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, 
   output: 'export',        // Enables static export
   images: {
     unoptimized: true,     // Disables image optimization (useful for static sites)
