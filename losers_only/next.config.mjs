9064a73fb3af91577c465d1c621396cdd33011e5
JayG <thatsumpossible@gmail.com>
2024-10-20 23:07:00 -0700

    more changes to config and yml

--- losers_only/next.config.mjs
+++ losers_only/next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,     // Disables image optimization (useful for static sites)
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',  // Point to the docs folder in production
+
 };
 
 export default nextConfig;
