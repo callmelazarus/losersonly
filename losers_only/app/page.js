771b7f9bcd45f4393466f4fcfb674e2074ec9920
JayG <thatsumpossible@gmail.com>
2024-10-21 22:38:22 -0700

    changed colors and rounded edges in flex box

--- losers_only/app/page.js
+++ losers_only/app/page.js
@@ -3,10 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div class="flex items-center justify-center h-screen bg-gray-100">
-<div class="w-64 h-64 bg-blue-500 text-white flex items-center justify-center animate-font-change">
+<div class="w-64 h-64 bg-blue-500 text-white rounded-md flex items-center justify-center animate-font-change">
 Losers Only.
   </div>
 </div>
-
   );
 }
--- losers_only/tailwind.config.js
+++ losers_only/tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  purge: ['./pages/**/*.js', 
-    './components/**/*.js',
-    './app/**/*.{js,ts,jsx,tsx}',  // Covers all files in the app directory
-
+  purge: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}", // Covers all files in the app directory
   ], // Adjust based on your file structure
   theme: {
     extend: {
@@ -18,15 +18,40 @@ module.exports = {
       },
       keyframes: {
         fontAnimation: {
-          '0%': { fontSize: '1.5rem', color: '#FAF9F6', fontWeight: 'normal', fontStyle: 'normal' },
-          '25%': { fontSize: '2rem', color: '#FAF9F6', fontWeight: 'lighter', fontStyle: 'italic' },
-          '50%': { fontSize: '2.5rem', color: '#ffffff', fontWeight: 'bold', fontStyle: 'normal' },
-          '75%': { fontSize: '2rem', color: '#FAF9F6', fontWeight: 'lighter', fontStyle: 'italic' },
-          '100%': { fontSize: '1.5rem', color: '#FAF9F6', fontWeight: 'normal', fontStyle: 'normal' },
+          "0%": {
+            fontSize: "1.5rem",
+            color: "#FAF9F6",
+            fontWeight: "normal",
+            fontStyle: "normal",
+          },
+          "25%": {
+            fontSize: "2rem",
+            color: "#FAF9F6",
+            fontWeight: "lighter",
+            fontStyle: "italic",
+          },
+          "50%": {
+            fontSize: "2.5rem",
+            color: "#ffffff",
+            fontWeight: "bold",
+            fontStyle: "normal",
+          },
+          "75%": {
+            fontSize: "2rem",
+            color: "#FAF9F6",
+            fontWeight: "lighter",
+            fontStyle: "italic",
+          },
+          "100%": {
+            fontSize: "1.5rem",
+            color: "#FAF9F6",
+            fontWeight: "normal",
+            fontStyle: "normal",
+          },
         },
       },
       animation: {
-        'font-change': 'fontAnimation 8s infinite',
+        "font-change": "fontAnimation 8s infinite",
       },
     },
   },
