9f339ea9fde0da7f0f5f9d66fd2a49acb5095a60
JayG <thatsumpossible@gmail.com>
2024-10-21 22:13:53 -0700

    updated config to remove assetPrefix

--- losers_only/next.config.mjs
+++ losers_only/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,     // Disables image optimization (useful for static sites)
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',  // Point to the docs folder in production
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',  // Point to the docs folder in production
 };
 
 export default nextConfig;
