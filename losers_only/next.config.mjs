5c79d352a8994cfa204bf2a26d3852bc4ac3ab9b
JayG <thatsumpossible@gmail.com>
2024-10-21 22:07:38 -0700

    more tried with yml file

--- losers_only/next.config.mjs
+++ losers_only/next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
     unoptimized: true,     // Disables image optimization (useful for static sites)
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',  // Point to the docs folder in production
-
 };
 
 export default nextConfig;
